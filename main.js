7dc7cad3d05e38815c5f548744429509eab40113
Matthew Knox <mrk45@uclive.ac.nz>
2016-11-27 11:21:55 +1300

    Fix configuration

--- main.js
+++ main.js
@@ -1,19 +1,18 @@
 const args = require('concierge/arguments'),
     ExamWaiter = require('./detect.js'),
     selector = require('./selector.js');
-let configurationData = null;
 
 const getResults = (year, semester, api, event) => {
-    if (!configurationData[event.sender_id]) {
+    if (!exports.config[event.sender_id]) {
         throw new Error('Please use "configure" first.');
     }
     if (year && semester) {
         api.sendMessage(`Getting results for ${year}-${semester}. Please note that if myuc is down this will not complete until it is up again.`, event.thread_id);
     }
-    const waiter = new ExamWaiter(configurationData[event.sender_id].username, configurationData[event.sender_id].password, !(year && semester));
+    const waiter = new ExamWaiter(exports.config[event.sender_id].username, exports.config[event.sender_id].password, !(year && semester));
     waiter.on('results', (results) => {
         if (!api) {
-            api = platform.getIntegrationApis()[configurationData[event.sender_id].source];
+            api = platform.getIntegrationApis()[exports.config[event.sender_id].source];
         }
         const actualResults = selector(results, year, semester);
         let res = '--------------------------------------------------------\nMark\tCourse\n';
@@ -29,50 +28,50 @@ const getResults = (year, semester, api, event) => {
 
     waiter.on('loginFailure', () => {
         if (!api) {
-            api = platform.getIntegrationApis()[configurationData[event.sender_id].source];
+            api = platform.getIntegrationApis()[exports.config[event.sender_id].source];
         }
         api.sendMessage(`Hmmmm.... ${event.sender_name}, either your login details were incorrect or something is broken.`, event.thread_id);
     });
-    if (!configurationData[event.sender_id].waiters) {
-        configurationData[event.sender_id].waiters = [];
+    if (!exports.config[event.sender_id].waiters) {
+        exports.config[event.sender_id].waiters = [];
     }
     if (year === null && semester === null) {
-        configurationData[event.sender_id].waiters.unshift(waiter);
+        exports.config[event.sender_id].waiters.unshift(waiter);
     }
     else {
-        configurationData[event.sender_id].waiters.push(waiter);
+        exports.config[event.sender_id].waiters.push(waiter);
     }
     waiter.start();
 };
 
 exports.load = (platform) => {
-    configurationData = exports.config.data || {};
-    for (let key in Object.keys(configurationData)) {
-        if (!configurationData[key].notify) {
+    for (let key in Object.keys(exports.config)) {
+        if (exports.config[key].waiters) {
+            delete exports.config[key].waiters;
+        }
+        if (!exports.config[key].notify) {
             continue;
         }
         const event = {
-            thread_id: configurationData[key].thread,
-            sender_id: configurationData[key].id,
-            sender_name: configurationData[key].name,
-            event_source: configurationData[key].source
+            thread_id: exports.config[key].thread,
+            sender_id: exports.config[key].id,
+            sender_name: exports.config[key].name,
+            event_source: exports.config[key].source
         };
         getResults(null, null, null, event);
     }
-    delete exports.config.data;
 };
 
 exports.unload = () => {
-    for (let key in Object.keys(configurationData)) {
-        if (!configurationData[key].waiters) {
+    for (let key in Object.keys(exports.config)) {
+        if (!exports.config[key].waiters) {
             continue;
         }
-        for (let waiter of configurationData[key].waiters) {
+        for (let waiter of exports.config[key].waiters) {
             waiter.stop();
         }
-        delete configurationData[key].waiters;
+        delete exports.config[key].waiters;
     }
-    exports.config.data = configurationData;
 };
 
 exports.run = (api, event) => {
@@ -87,7 +86,7 @@ exports.run = (api, event) => {
                     out.log('What are you doing? I need a username and password.');
                 }
                 else {
-                    configurationData[event.sender_id] = {
+                    exports.config[event.sender_id] = {
                         source: event.event_source,
                         id: event.sender_id,
                         name: event.sender_name,
@@ -103,13 +102,13 @@ exports.run = (api, event) => {
             short: 'd',
             description: 'Delete all configuration for your user.',
             run: (out) => {
-                if (configurationData[event.sender_id]) {
-                    if (configurationData[key].waiters) {
-                        for (let waiter of configurationData[key].waiters) {
+                if (exports.config[event.sender_id]) {
+                    if (exports.config[key].waiters) {
+                        for (let waiter of exports.config[key].waiters) {
                             waiter.stop();
                         }
                     }
-                    delete configurationData[event.sender_id];
+                    delete exports.config[event.sender_id];
                     out.log(`Configuration for '${event.sender_name}' has been deleted.`);
                 }
                 else {
@@ -136,11 +135,11 @@ exports.run = (api, event) => {
             short: 'n',
             description: 'Notifies you when results for the current exams are out.',
             run: (out) => {
-                if (configurationData[event.sender_id].notify) {
+                if (exports.config[event.sender_id].notify) {
                     throw new Error('I am already going to notify you...');
                 }
-                configurationData[event.sender_id].notify = true;
-                configurationData[event.sender_id].thread = event.thread_id;
+                exports.config[event.sender_id].notify = true;
+                exports.config[event.sender_id].thread = event.thread_id;
                 out.log('Will do.');
                 getResults(null, null, api, event);
             }
@@ -150,12 +149,12 @@ exports.run = (api, event) => {
             short: 's',
             description: 'Disables the notification when results for the current exams are out.',
             run: (out) => {
-                if (!configurationData[event.sender_id].notify) {
+                if (!exports.config[event.sender_id].notify) {
                     throw new Error('I wasn\'t going to notify you anyway...');
                 }
-                delete configurationData[event.sender_id].notify;
+                delete exports.config[event.sender_id].notify;
                 out.log('Consider it stopped.');
-                const notify = configurationData[event.sender_id].waiters.shift();
+                const notify = exports.config[event.sender_id].waiters.shift();
                 notify.stop();
             }
         }
